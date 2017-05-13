2870ff443261ae1d4a490d6c72c85671b0629f56
Wes Etheredge <jwe@metastudiosinc.com>
2017-05-13 14:43:41 -0400

    server upgrade and react linking

---
+++ source/app.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import {render} from "react-dom"
+import CoreContent from "./components/CoreContent.react.js"
+
+render(<CoreContent />, document.getElementById('react-application'))
---
+++ source/components/CoreContent.react.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+
+class CoreContent extends React.Component {
+  render() {
+    return <h1>Hello Rachel</h1>;
+  }
+}
+
+export default CoreContent;
